b7a849304ee3c6c6379edd1544e512ee2dbd349c
xin <709464835@qq.com>
2024-09-14 16:07:47 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("ok")
+import express, {Request, Response} from "express";
+import bodyParser from "body-parser";
+
+import router from "./router"
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false})); // 解析请求内容
+
+app.use(router);
+
+
+app.listen(8080, ()=>{
+    console.log("server is running is :8080")
+})
---
+++ src/router.ts
@@ -0,0 +1,37 @@
+import { Router, Request, Response} from "express";
+
+const router = Router();
+
+router.get("/", (req:Request, res: Response) =>{
+    res.send(`<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <form action="/getData" method="post">
+        <input type="password" name="password">
+        <button>提交</button>
+    </form>
+</body>
+</html>`)
+})
+
+
+interface RequestWithBody extends Request{
+    body: {
+        // password: string | undefined
+        [key: string]: string | undefined
+    }
+}
+
+router.post("/getData", (req:RequestWithBody, res: Response) =>{
+    const {password, name} = req.body;
+    if (password === "xinxin") {
+        res.send("success")
+    } else{
+        res.send("error")
+    }
+}) 
+
+export default router
